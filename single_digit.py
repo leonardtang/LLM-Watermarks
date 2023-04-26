6ced58a10c2977ad85f26f9896cc98fd75a3ec51
Leonard Tang <leonardgentwintang@gmail.com>
2023-04-26 13:22:21 -0700

    Attempt to integrate Alapaca-LoRA

--- single_digit.py
+++ single_digit.py
@@ -13,10 +13,10 @@ import torch
 from api_keys import OPENAI_API_KEY
 from collections import defaultdict
 from watermark_playground import SingleLookbackWatermark
-from transformers import GPT2Tokenizer, GPT2LMHeadModel
+from transformers import GPT2Tokenizer, GPT2LMHeadModel, LlamaTokenizer, LlamaForCausalLM
 
 openai.api_key = OPENAI_API_KEY
-device = torch.device("cuda:2") if torch.cuda.is_available() else torch.device("cpu")
+device = torch.device("cuda:4") if torch.cuda.is_available() else torch.device("cpu")
 print("Device:", device)
 
 
@@ -31,22 +31,28 @@ def generate_from_model(
     logits_processors = [],
 ):
     if decode == 'beam':
-        print("if decode == 'beam':")
+        print("decode is beam")
+        # TODO(ltang): refactor multinomial/greedy into here
         outputs = model.generate(
             input_ids,
             min_length=length,
-            max_length=length,
+            max_new_tokens=length,
             num_beams=num_beams,
             repetition_penalty=repetition_penalty,
             eos_token_id=tokenizer.eos_token_id,
-            pad_token_id=tokenizer.pad_token_id,
+            pad_token_id=tokenizer.eos_token_id,
             logits_processor=logits_processors,
         )
-        return outputs
+        # TODO(ltang): postprocess and check for first occurence of a number
+        generated_text = tokenizer.decode(outputs[0], skip_special_tokens=True)
+        print("GEnerated text", generated_text)
+        return generated_text
     
     else: 
+        print("decode is NOT beam")
         with torch.no_grad():
-            for _ in range(length):    
+            for _ in range(length):
+                print("decoding one time step")    
                 logits = model(input_ids)
                 if watermark:
                     mask = watermark(input_ids) 
@@ -65,6 +71,7 @@ def generate_from_model(
                     raise Exception
 
                 # TODO(ltang): think about if we need to consider shifting distribution of whitespace (e.g. \n, \t)
+                print("Sample:", sample)
                 if sample.item().isnumeric():
                     return int(sample.item())
 
@@ -72,8 +79,8 @@ def generate_from_model(
                 # Affects masker/watermark via `hash_tensor``
                 input_ids = torch.cat([input_ids, sample.unsqueeze(0)], dim=1)
 
-            # No digit produced
-            return None
+            # No digit produced. Just return number for now.
+            return -50
 
 
 def generate_random_digit(
@@ -123,22 +130,31 @@ def generate_random_digit(
         print("elif model_name == 'gpt-2':")
         input_ids = torch.tensor(tokenizer.encode(prompt)).unsqueeze(0).to(device)
         processor = [watermark] if watermark else []
-        generate_from_model(model, input_ids, tokenizer, length, decode, logits_processors=processor)
+        generate_from_model(model, input_ids, tokenizer, length, decode, logits_processors=processor, repetition_penalty=1.3)
         
+    elif model_name == 'alpaca-lora':
+        print("elif model_name == 'alpaca-lora':")
+        input_ids = torch.tensor(tokenizer.encode(prompt)).unsqueeze(0).to(device)
+        processor = [watermark] if watermark else []
+        generate_from_model(model, input_ids, tokenizer, length, decode, logits_processors=processor, repetition_penalty=1)
     else: 
         pass
 
 
 def plot_digit_frequency(digits):
 
+    print("Raw digits!", digits)
     # Construct dict for numerical reference
     digit_counts = {}
     # TODO(ltang): don't hardcode this range in the future
     for i in range(100):
         digit_counts[i] = 0 
 
-    for d in digits: 
-        digit_counts[d] += 1
+    for d in digits:
+        if d in set(range(100)): 
+            digit_counts[d] += 1
+        else:
+            digit_counts[d] = 500
 
     with open('digit_counts.json', 'w') as file:
         json.dump(digit_counts, file, indent=4)
@@ -151,29 +167,42 @@ def plot_digit_frequency(digits):
     plt.savefig('digit_freq.png')
 
 
-def repeatedly_sample(prompt, model_name, engine="text-davinci-003", repetitions=2000):
+def repeatedly_sample(prompt, model_name, engine="text-davinci-003", decode='beam', length=10, repetitions=2000):
 
     if model_name == 'openai-api':
         tokenizer = None
     elif model_name == 'gpt-2':
-        print("Loading model...")
         tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
-        print("Tokenizer loaded!")
-        # model = GPT2LMHeadModel.from_pretrained('gpt2-xl').to(device)
-        model = GPT2LMHeadModel.from_pretrained('gpt2-large').to(device)
+        model = GPT2LMHeadModel.from_pretrained('gpt2-xl').to(device)
         vocab_size = model.config.vocab_size
+    elif model_name == 'alpaca-lora':
+        tokenizer = LlamaTokenizer.from_pretrained("chainyo/alpaca-lora-7b")
+        model = LlamaForCausalLM.from_pretrained(
+            "chainyo/alpaca-lora-7b",
+            load_in_8bit=True,
+            torch_dtype=torch.float16,
+            device_map="auto",
+        )
+
     
     print(f"Sampling for {repetitions} repetitions")
     sampled_digits = []
     for _ in range(repetitions):
-        d = generate_random_digit(prompt, tokenizer, model_name, model=model, engine=engine)
+        d = generate_random_digit(prompt, tokenizer, model_name, model=model, length=length, decode=decode, engine=engine)
         sampled_digits.append(d)
     
     return sampled_digits
 
 
 if __name__ == "__main__":
-    prompt = "Pick a random number between 1 and 100. Just return the number, don't include any other text or punctuation in the response."
-    digit_sample = repeatedly_sample(prompt, 'gpt-2', engine=None, repetitions=200)
+    # prompt = "What is a random value between 1 and 100?"
+    # Alpaca Prompt
+    prompt = """Below is an instruction that describes a task. Write a response that appropriately completes the request.
+
+    ### Instruction:
+    Generate a random number between 1 and 100.
+
+    ### Response:"""
+    digit_sample = repeatedly_sample(prompt, 'alpaca-lora', engine=None, decode='beam', length=500, repetitions=100)
     plot_digit_frequency(digit_sample)
 
