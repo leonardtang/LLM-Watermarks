d7c7c5a31dd8555938a277170f1d3d0b3f22ebd7
Leonard Tang <leonardgentwintang@gmail.com>
2023-05-02 00:49:42 -0700

    K-S analysis

---
+++ kolmogorov.py
@@ -0,0 +1,64 @@
+"""
+Two-sample Kolmogorov-Smirnov test between 1-100 RNG distribution of watermarked vs. unmarked LLMs
+"""
+
+import json
+import numpy as np
+import os
+from scipy import stats
+
+
+def ks_test(unmarked_file, watermarked_file):
+    
+    unmarked_dist = np.concatenate(np.load(unmarked_file))
+    watermarked_dist = np.concatenate(np.load(watermarked_file))
+
+    assert len(unmarked_dist) == len(watermarked_dist)
+    print(f"Unmarked File: {unmarked_file}")
+    print(f"Watermarked File: {watermarked_file}")
+    print(f"Total Sample Size: {len(unmarked_dist)}")
+
+    # Hope this can just take in np.arrays
+    test_res = stats.kstest(unmarked_dist, watermarked_dist)
+    return test_res.statistic, test_res.pvalue
+
+
+def iter_dir(dir_name='rng'):
+    
+    # TODO(ltang): clean up this hacky ass code
+    for filename in os.listdir(dir_name):
+        if filename.endswith('.npy'):
+            file_path = os.path.join(dir_name, filename)
+            if 'alpaca' in filename and 'unmarked' in filename:
+                alpaca_unmarked_fp = file_path
+            if 'flan' in filename and 'unmarked' in filename:
+                flan_unmarked_fp = file_path
+            
+    # Actually do tests
+    alpaca_ks = {}
+    flan_ks = {}
+
+    # Fix this later LOL, testing since we don't have unmarked RNG files for now
+    alpaca_unmarked_fp = 'rng/alpaca_marked_g10_d1_rep_10_4-28-17-36.npy'
+    flan_unmarked_fp = 'rng/flan_marked_g10_d1_rep_10_4-28-18-2.npy'
+
+    for filename in os.listdir(dir_name):
+        if filename.endswith('.npy'):
+            file_path = os.path.join(dir_name, filename)
+            if 'alpaca' in filename:
+                stat, p_val = ks_test(alpaca_unmarked_fp, file_path)
+                alpaca_ks[filename] = p_val
+            if 'flan' in filename:
+                stat, p_val = ks_test(flan_unmarked_fp, file_path)
+                flan_ks[filename] = p_val
+
+    
+    with open('alpaca_ks.json', "w") as outfile:
+        json.dump(alpaca_ks, outfile, indent=4)
+    
+    with open('flan_ks.json', "w") as outfile:
+        json.dump(flan_ks, outfile, indent=4)
+
+
+if __name__ == "__main__":
+    iter_dir()
---
+++ logit_rng.py
@@ -0,0 +1,5 @@
+"""
+Generate 1-100 random numbers directly from logit file.
+Namely, this is a valid approach since the prompt is fixed.
+"""
+
